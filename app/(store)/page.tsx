55f85358795d050b9e2af38e72d72d13853cc5b4
Subhan-anwer <sheikhanwer30@gmail.com>
2025-01-29 16:07:05 +0500

    Initial Commit

---
+++ app/(store)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div >
+      <h1>Hello world!!! MY dReam</h1>
+      <Button className="active:scale-90 transition-transform duration-200">Click me</Button>
+    </div>
+  );
+}
