e33ef507ec8c6048f14e6b16a0557a6b6b0b8ff4
Subhan-anwer <sheikhanwer30@gmail.com>
2025-01-31 10:24:35 +0500

    Search page 1

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="bg-slate-300 p-9 flex items-center justify-center flex-col gap-3" >
       <h1>Hello world!!!</h1>
-      {/* <Button className="active:scale-90 transition-transform duration-200">Click me</Button> */}
+      <Button className="active:scale-90 transition-transform duration-200">Click me</Button>
     </div>
   );
 }
--- app/(store)/search/page.tsx
+++ app/(store)/search/page.tsx
@@ -1,6 +1,11 @@
-const SearchPage = () => {
+const SearchPage = async ({ searchParams }: { searchParams: { query: string } }) => {
+
+  const { query } = await searchParams
+
   return (
-    <div>SearchPage</div>
+    <div>
+      <h3 className="font-normal text-lg">Your Search is "<span className="font-bold text-lg">{query}</span>"</h3>
+    </div>
   )
 }
 
