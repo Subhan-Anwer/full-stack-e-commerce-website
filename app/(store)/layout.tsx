7c3519b7f6febcd1af7477ce8f0542bd67fa9343
Subhan <sheikhanwer30@gmail.com>
2025-03-06 00:30:22 +0500

    added visual editor

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -6,13 +6,14 @@ import Header from "../../components/Header"
 import { SanityLive } from "@/sanity/lib/live";
 import { VisualEditing } from "next-sanity";
 import { draftMode } from "next/headers";
+import { DisableDraftmode } from "@/components/DisableDraftmode";
 
 export const metadata: Metadata = {
   title: "Shopit ",
   description: "Developed by Subhan Anwer",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -21,6 +22,12 @@ export default function RootLayout({
     <ClerkProvider dynamic>
       <html lang="en">
         <body className="min-h-screen">
+          {(await draftMode()).isEnabled && (
+            <>
+              <DisableDraftmode />
+              <VisualEditing />
+            </>
+          )}
           <main>
             <Header />
             {children}
--- app/draft-mode/disable/route.ts
+++ app/draft-mode/disable/route.ts
@@ -4,6 +4,6 @@ import { draftMode } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-    await(await draftMode()).disable();
+    await (await draftMode()).disable();
     return NextResponse.redirect(new URL("/", request.url));
 }
